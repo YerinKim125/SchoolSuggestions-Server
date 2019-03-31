c13aa82acbed5b4675f92667be8fe7bf6c689433
yerin <kimyerin125@gmail.com>
2019-03-31 15:40:21 +0900

    initial commit

---
+++ src/main/java/com/contest/schoolsuggestions/SchoolsuggestionsApplication.java
@@ -0,0 +1,13 @@
+package com.contest.schoolsuggestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolsuggestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolsuggestionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/contest/schoolsuggestions/SchoolsuggestionsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.contest.schoolsuggestions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchoolsuggestionsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
